1800e083a3aed91071aa57f77c447d56314d6b78
Qoushik <qstar.inc@gmail.com>
2024-05-04 00:41:56 +0600

    Add project files.

---
+++ Mod.cs
@@ -0,0 +1,20 @@
+﻿using Colossal.Logging;
+using Game;
+using Game.Modding;
+
+namespace StarQ_Vanilla_Ploppable_Service_Buildings
+{
+    public class Mod : IMod
+    {
+        public static ILog log = LogManager.GetLogger($"{nameof(StarQ_Vanilla_Ploppable_Service_Buildings)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            log.Info($"Current mod asset at {nameof(StarQ_Vanilla_Ploppable_Service_Buildings)}");
+        }
+
+        public void OnDispose()
+        {
+        }
+    }
+}
