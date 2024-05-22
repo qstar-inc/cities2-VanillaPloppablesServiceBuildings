ae8ce92f186b3df46755c806bfc76bda8e63d9f0
Qoushik <qstar.inc@gmail.com>
2024-05-22 18:12:58 +0600

    plot size fix

--- Mod.cs
+++ Mod.cs
@@ -1,16 +1,16 @@
-﻿using Colossal.Logging;
+﻿//using Colossal.Logging;
 using Game;
 using Game.Modding;
 
-namespace StarQ_Vanilla_Ploppable_Service_Buildings
+namespace VanillaPloppableServiceBuildings
 {
     public class Mod : IMod
     {
-        public static ILog log = LogManager.GetLogger($"{nameof(StarQ_Vanilla_Ploppable_Service_Buildings)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+        //public static ILog log = LogManager.GetLogger($"{nameof(VanillaPloppableServiceBuildings)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
 
         public void OnLoad(UpdateSystem updateSystem)
         {
-            log.Info($"Current mod asset at {nameof(StarQ_Vanilla_Ploppable_Service_Buildings)}");
+            //log.Info($"Current mod asset at {nameof(VanillaPloppableServiceBuildings)}");
         }
 
         public void OnDispose()
